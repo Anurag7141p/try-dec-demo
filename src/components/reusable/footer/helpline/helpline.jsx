db485593ec1332bfc7894ee1742c2c3129eb5a15
anurag <anuragnbr@gmail.com>
2024-03-08 01:28:08 +0530

    helpline banner responsive

--- src/components/reusable/footer/helpline/helpline.jsx
+++ src/components/reusable/footer/helpline/helpline.jsx
@@ -4,7 +4,7 @@ import FooterImage1 from "../../../../assets/footerImages/FooterImage1.jpg";
 export default function Helpline() {
   return (
     <div
-      className="mb-10 h-[308px] w-full rounded-2xl text-black outline outline-white"
+      className="mb-10 xl:h-[308px] lg:h-[308px] md:h-[200px] w-full rounded-2xl text-black outline outline-white"
       style={{
         backgroundImage: `url("https://res.cloudinary.com/dgkg64nql/image/upload/v1709729380/trydec/helpline.png")`,
         backgroundRepeat: "no-repeat",
@@ -15,12 +15,12 @@ export default function Helpline() {
       <div className="lg:flex  items-center  lg:mx-6 lg:gap-3  pl-4  rounded-lg space-x-4 lg:justify-normal lg:items-center h-full">
         <div className="flex flex-col gap-10">
           <div className="flex flex-col gap-4">
-            <h3 className="text-3xl lg:ms-20  font-bold">Olapool helpline</h3>
-            <p className="  lg:ms-20">
+            <h3 className="xl:text-3xl lg:text-2xl md:text-xl lg:ms-20  md:ms-10 font-bold">Olapool helpline</h3>
+            <p className="  lg:ms-20 md:ms-10">
               Provides support for all your service needs.
             </p>
           </div>
-          <button className="bg-green-500 lg:ms-20  px-4 py-2 rounded-md text-white">
+          <button className="bg-green-500 lg:ms-20 md:ms-10 md:w-1/2  px-4 py-2 rounded-md text-white">
             Get Call Back
           </button>
         </div>
