d526a5fae413b7c2f0c6e70576b673fce82b1799
anurag <anuragnbr@gmail.com>
2024-03-08 19:09:49 +0530

    rental vehicle padding added

--- src/components/homePage/rentalVehicle.jsx
+++ src/components/homePage/rentalVehicle.jsx
@@ -4,9 +4,9 @@ import ImageCard from '../../components/reusable/card/homecard2';
 import cameraData from '../../data/landingpagedata/bestRentalData/cameraData';
 const RentalVehicle = () => {
   return ( 
-    <div className="p-4 justify-center items-center ">
+    <div className=" justify-center items-center px-10">
       <div className="flex justify-between">
-        <h2 className="text-2xl font-bold px-4 lg:ms-8 text-lg">Rental Vechiles</h2>
+        <h2 className=" font-bold px-4 lg:ms-8 text-lg">Rental Vechiles</h2>
         <Link to={"/"} className="cursor-pointer ">See All</Link>
       </div>
       <div className="sm:flex overflow-x-auto col-lg-2 col-md-3 col-sm-4 col-8 mb-4">
